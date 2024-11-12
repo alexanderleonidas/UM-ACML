b08854055d4ae302b5a0b18bf0f11d394f4c244b
alexanderleonidas <alexanderleonidas@me.com>
2024-11-12 12:56:15 +0100

    added files for next task

---
+++ autoencoding/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+from torch.autograd import Variable
+
+# Import the dataset
